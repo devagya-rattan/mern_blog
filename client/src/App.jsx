f6b094352cda7c0ab28a4ba46076f569d2eaf789
devagya <rattandevagya@gmail.com>
2024-02-02 20:03:16 +0530

    work done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer"
 import Signin from "./pages/Signin";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
@@ -16,6 +17,7 @@ function App() {
       <Route path='/about' element={<About />} />
       <Route path='/projects' element={<Projects />} />
       <Route path="/signin" element={<Signin/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,45 +1,40 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  signInStart,
-  signInSuccess,
-  signInFailure,
-} from '../redux/user/userSlice';
 // import OAuth from '../components/OAuth';
 
-export default function SignIn() {
+export default function SignUp() {
   const [formData, setFormData] = useState({});
-  const { loading, error: errorMessage } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!formData.email || !formData.password) {
-      return dispatch(signInFailure('Please fill all the fields'));
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
     }
     try {
-      dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
       if (data.success === false) {
-        dispatch(signInFailure(data.message));
+        return setErrorMessage(data.message);
       }
-
-      if (res.ok) {
-        dispatch(signInSuccess(data));
-        navigate('/');
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
       }
     } catch (error) {
-      dispatch(signInFailure(error.message));
+      setErrorMessage(error.message);
+      setLoading(false);
     }
   };
   return (
@@ -54,7 +49,7 @@ export default function SignIn() {
             Blog
           </Link>
           <p className='text-sm mt-5'>
-            This is a demo project. You can sign in with your email and password
+            This is a demo project. You can sign up with your email and password
             or with Google.
           </p>
         </div>
@@ -62,6 +57,15 @@ export default function SignIn() {
 
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value='Your username' />
+              <TextInput
+                type='text'
+                placeholder='Username'
+                id='username'
+                onChange={handleChange}
+              />
+            </div>
             <div>
               <Label value='Your email' />
               <TextInput
@@ -75,7 +79,7 @@ export default function SignIn() {
               <Label value='Your password' />
               <TextInput
                 type='password'
-                placeholder='**********'
+                placeholder='Password'
                 id='password'
                 onChange={handleChange}
               />
@@ -91,15 +95,15 @@ export default function SignIn() {
                   <span className='pl-3'>Loading...</span>
                 </>
               ) : (
-                'Sign In'
+                'Sign Up'
               )}
             </Button>
             {/* <OAuth /> */}
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Dont Have an account?</span>
-            <Link to='/signup' className='text-blue-500'>
-              Sign Up
+            <span>Have an account?</span>
+            <Link to='/signin' className='text-blue-500'>
+              Sign In
             </Link>
           </div>
           {errorMessage && (
