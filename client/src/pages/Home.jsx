fbace6de5200f03003b06c33fb1730095cc40aad
devagya <rattandevagya@gmail.com>
2024-02-04 12:04:22 +0530

    doing still doing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CallToaction from "../components/CallToaction";
+// import { useState, useEffect } from "react";
+// import PostCard from "../components/PostCard";
 const Home = () => {
+  // const [posts, setPosts] = useState([]);
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const res = await fetch("/api/post/getPosts");
+  //     const data = res.json();
+  //     setPosts(data.posts);
+  //   };
+  //   fetchPosts();
+  // }, []);
   return (
     <>
       <div>
@@ -21,6 +32,24 @@ const Home = () => {
         <div className="p-3 bg-amber-100 dark:bg-slate-700">
           <CallToaction />
         </div>
+        {/* <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='flex flex-wrap gap-4'>
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div> */}
       </div>
     </>
   );
